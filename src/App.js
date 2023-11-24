60294fcee1a75dacef8d75db6aebb84ff680a5b6
RaoAmmar027 <151843804+SpencerTyson@users.noreply.github.com>
2023-11-24 16:07:57 +0500

    restaurant
    react hooks are also explained

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Restaurants from './Components/Basics/Restaurants'
+import UseState  from './Components/Basics/Hooks/UseState'
+import UseEffect  from './Components/Basics/Hooks/useEffect'
+import UseReducer from './Components/Basics/Hooks/useReducer'; 
+import Todo from './TodoReact/Todo';
+
+const App = () => {
+    
+    //return <Todo />;
+    //return <UseReducer />;   
+    //return <UseEffect />;
+    //return <UseState />;
+   return <Restaurants />;
+}
+
+export default App;
